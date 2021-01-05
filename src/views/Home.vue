be0ea2f1e38edc53159ea744d967339eb07fab64
“BANDER <Bandersaleh93@gmail.com>
2021-01-04 17:30:28 -0800

    Vuejs Frontend Axios request is showing the backend Get Routes successfully, missing edit/create/delete functions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1>All Professors</h1>
+    <div v-for="professor in professors">
+      <h2>{{ professor.name }}</h2>
+      <!-- <img v-bind:src="photo.url" v-bind:alt="photo.name" />
+      <p>Width: {{ photo.width }}</p>
+      <p>Height: {{ photo.height }}</p> -->
+    </div>
   </div>
 </template>
 
@@ -8,13 +14,24 @@
 </style>
 
 <script>
+import axios from "axios";
+
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      professors: [],
     };
   },
-  created: function() {},
-  methods: {}
+  created: function() {
+    this.indexPhotos();
+  },
+  methods: {
+    indexPhotos: function() {
+      axios.get("/professors/").then(response => {
+        console.log("photos index", response);
+        this.professors = response.data;
+      });
+    },
+  },
 };
 </script>
